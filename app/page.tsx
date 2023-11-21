e91dad2bfdd0d3a175fa0cd01676679a4e7bf371
Anshuman Sahu <anshuman2601@gmail.com>
2023-11-21 01:34:49 -0500

    Added coolcar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image src="/coolcar.jpeg" alt="car" width={850} height={500} style={{objectFit:"cover", borderRadius:"100px"}}/>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
